adf388315972eb94c0035a18117734c7bf49c53a
DaniloKasalica <danilo.kasalica@gmail.com>
2020-04-12 00:27:54 +0200

    added top rated, top rated and slider need model winodw

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ var slider = document.getElementById('slide')
 var next = document.getElementById('next')
 var prev = document.getElementById('prev');
 
-var req = new Request('https://newsapi.org/v2/top-headlines?country=us&apiKey=312463b31a7c4c65992ccb16c86425c2')
-fetch(req)
+
+fetch('https://newsapi.org/v2/everything?q=all&from=2020-04-10&to=2020-04-10&sortBy=popularity&apiKey=312463b31a7c4c65992ccb16c86425c2')
 .then(resp=>{
     if (resp.ok){
         return resp.json();
@@ -80,3 +80,81 @@ function goprev(){
 }
 setInterval(gonext,3000)
 
+fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=312463b31a7c4c65992ccb16c86425c2')
+.then(resp=>{
+    if (resp.ok){
+        return resp.json();
+
+    }
+    else 
+    throw new Error('err')
+    
+})
+.then(date=>{
+   
+    var topr=document.getElementById('toprated')
+    var sec = document.getElementById('sect');
+    var k = 0;
+    for(let i=0;i<2;i++){
+        var div = document.createElement('div');
+        div.className='row';
+        topr.appendChild(div);
+        for(j=0;j<3;j++){
+           var div_2=document.createElement('div');
+           div_2.className='item2';
+           div.appendChild(div_2);
+           var img = document.createElement('img');
+           img.setAttribute('src',date.articles[k].urlToImage);
+           div_2.appendChild(img);
+           var info = document.createElement('div');
+           info.className='rightpart'
+           div_2.appendChild(info)
+           var title = document.createElement('p');
+           title.innerHTML='Title:'+date.articles[k].title.substring(0,25)+'...';
+           var description = document.createElement('p');
+           description.innerHTML='Description:'+date.articles[k].description.substring(0,30)+'...';
+           var pub = document.createElement('p');
+           pub.innerHTML='Date:'+date.articles[k++].publishedAt;
+           info.appendChild(title)
+           info.appendChild(description)
+           info.appendChild(pub)
+           
+        }
+
+    }
+    var loadmorebtn=document.getElementById('loadmorebtn');
+    loadmorebtn.addEventListener('click',load);
+    function load(){
+        console.log(date.articles.length)
+        if(k>=date.articles.length-2){ 
+    
+            loadmorebtn.setAttribute('disabled',true)
+        }
+            var div = document.createElement('div');
+            div.className='row';
+            topr.appendChild(div);
+            for(j=0;j<3;j++){
+               var div_2=document.createElement('div');
+               div_2.className='item2';
+               div.appendChild(div_2);
+               var img = document.createElement('img');
+               img.setAttribute('src',date.articles[k].urlToImage);
+               div_2.appendChild(img);
+               var info = document.createElement('div');
+               info.className='rightpart'
+               div_2.appendChild(info)
+               var title = document.createElement('p');
+               title.innerHTML='Title:'+date.articles[k].title.substring(0,25)+'...';
+               var description = document.createElement('p');
+               description.innerHTML='Description:'+date.articles[k].description.substring(0,30)+'...';
+               var pub = document.createElement('p');
+               pub.innerHTML='Date:'+date.articles[k++].publishedAt;
+               info.appendChild(title)
+               info.appendChild(description)
+               info.appendChild(pub)
+               
+            }
+    
+        
+    }
+})
