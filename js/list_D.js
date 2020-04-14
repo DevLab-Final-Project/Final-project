5ac6fcd37c1a3b604dab6c2741aeb9582baf333d
DaniloKasalica <danilo.kasalica@gmail.com>
2020-04-14 20:46:28 +0200

    added nums but for list but without eventlistener

--- js/list_D.js
+++ js/list_D.js
@@ -1,4 +1,9 @@
 
+
+
+
+var res=localStorage.getItem("Category")
+
 var date = document.getElementById('date');
 date.addEventListener('change',filter)
  var form = document.getElementById('form')
@@ -20,6 +25,7 @@ function search(){
     day_1=parseInt(day_2)-start;
     var year = dateObj.getFullYear();
     var startDate = year + '-' + month + '-' + day_1;
+    day_2=parseInt(day_2);
     var finishDate = year + '-' + month + '-' + day_2;
     
     var list = document.getElementById('NUMS');
@@ -36,7 +42,8 @@ function search(){
 
 
   
-    uri = 'https://newsapi.org/v2/everything?q=sport&from='+startDate+'&to='+finishDate+'&sortBy=popularity&pageSize=100&apiKey=312463b31a7c4c65992ccb16c86425c2'
+    uri = 'https://newsapi.org/v2/everything?q='+res+'&from='+startDate+'&to='+startDate+'&pageSize=100&apiKey=312463b31a7c4c65992ccb16c86425c2'
+
     
 fetch(uri)
 .then(req=>{
@@ -55,24 +62,37 @@ fetch(uri)
     var k =0
     var checkk=0;
     
+    var arrnum= [];
+    var br2=0;
+    var k1=0;
+
+    if(name.checked){
+        for(let i=0;i<100;i++){
+
+          if(date.articles[k1].title.toLowerCase().startsWith(text.toLowerCase()) ){
+              
+            arrnum[br2++]=k1++;
+        
 
 
+        }
+        else{
+            k1++;
 
+        }
+     }
     var nums  = document.getElementById('NUMBTN')
     show3()
     
     function show3(){
-        main.innerHTML='';
-    if(name.checked){
-        
-    nums.innerHTML=''
-        for(let j = 1; j<=Math.floor(date.articles.length/4);j++)
-        {
-            var p = document.createElement('p');
-            p.textContent=j;
-            nums.appendChild(p)
+    
 
-        }
+        main.innerHTML='';
+
+
+
+
+        if(name.checked){
     for(let i = 0;i<=3;i++){ 
     
         if(k>=100){
@@ -124,6 +144,7 @@ fetch(uri)
         }
     }
 }
+
 var source = document.getElementById('SOURCE');
 if(source.checked){
     for(let i = 0;i<=3;i++){
@@ -238,7 +259,27 @@ if(auth.checked){
         }
     }
 }
+nums.innerHTML=''
+if(arrnum.length%4!=0){
+for(let j = 1; j<=Math.floor(arrnum.length/4+1);j++)
+{
+    var p = document.createElement('p');
+    p.textContent=j;
+    nums.appendChild(p)
+   // p.addEventListener('click',gotonum)
 
+}
+}
+else{
+    for(let j = 1; j<=Math.floor(arrnum.length/4);j++)
+{
+    var p = document.createElement('p');
+    p.textContent=j;
+    nums.appendChild(p)
+  //  p.addEventListener('click',gotonum)
+
+}
+}
 if(arr.length<5){
 
     prev.style.opacity= '0.5'
@@ -251,8 +292,7 @@ if(arr.length<5){
     prev.style.opacity= '1'
      prev.style.cursor='pointer'
  }
- 
- if(checkk<1){
+ if(checkk<1 && arr.length!=arrnum.length){
  next.addEventListener('click', gonext);
  next.style.opacity= '1'
  next.style.cursor='pointer'
@@ -265,22 +305,29 @@ if(arr.length<5){
        }
 
     }
+
+
+
+
+    var a;
+
     function gonext(){
-        console.log(arr)
         show3()
     }
+
+
+
 function goprev(e){
-    
     var  countchild = main.childElementCount;
     if(countchild==4){
-    console.log(arr)
     next.style.opacity= '1'
     next.style.cursor='pointer'
 
     next.addEventListener('click',gonext )
     e.target.style.opacity='1'
+
+
     k=arr[arr.length-8]
-    console.log(k)
     br=br-8;
     arr.pop()
     arr.pop()
@@ -318,6 +365,17 @@ function goprev(e){
 }
 
 
+
+//function gotonum(e){
+//  a = parseInt(e.target.textContent)
+  //  k=arrnum[a*4]
+   // br=(a*4-1);
+  //  arr.length=br+4;
+ //   console.log(k)
+ //   show3()
+//}
+
+}
 })
 }
 function filter(e){
